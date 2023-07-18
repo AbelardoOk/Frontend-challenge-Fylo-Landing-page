bfd41148a0b51496b9431aab759329dc517ed5ee
Abelardo Palácios <71283403+AbelardoOk@users.noreply.github.com>
2023-07-18 09:27:11 -0400

    fix: fixed some ui components

--- src/components/contactPage.tsx
+++ src/components/contactPage.tsx
@@ -7,18 +7,27 @@ export function EarlyAcessCard() {
         our support team would be happy to help you
       </p>
 
-      <div className="flex flex-col md:flex-row gap-6 w-full justify-between">
-        <input
-          className="rounded-3xl px-6 md:w-2/3 w-full py-4"
-          type="text"
-          name=""
-          id=""
-          placeholder="email@example.com"
-        />
-        <button className="py-4 px-7 font-alt font-bold rounded-3xl bg-accent-blue hover:bg-accent-cyan">
+      <form className="flex flex-col md:flex-row gap-6 w-full justify-between items-center md:items-start">
+        <div className="w-full md:w-2/3">
+          <input
+            className="rounded-3xl w-full md:w-full px-6 py-4 text-primary-testimonials border-0 valid:border-accent-cyan valid:border-2 invalid:border-accent-red invalid:border-2"
+            type="email"
+            placeholder="email@example.com"
+            pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
+            required
+          />
+          <p className="text-xs text-transparent invalid:text-accent-red font-bold text-accent-red text-left pt-2 pl-6">
+            Please enter a valid email adress
+          </p>
+        </div>
+
+        <button
+          className="py-4 px-7 font-alt font-bold rounded-3xl bg-accent-blue hover:bg-accent-cyan transition duration-300"
+          type="submit"
+        >
           Get Started For Free
         </button>
-      </div>
+      </form>
     </div>
   );
 }
