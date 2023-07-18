d5ccd9eeca1b87e65c2395887a2509c4b974b988
Abelardo Palácios <71283403+AbelardoOk@users.noreply.github.com>
2023-07-17 23:19:26 -0400

    feat: created mobile version of site

--- src/components/contactPage.tsx
+++ src/components/contactPage.tsx
@@ -1,14 +1,20 @@
 export function EarlyAcessCard() {
   return (
-    <div className="flex flex-col py-12 px-28 mt-32 mx-[17.5rem] bg-primary-testimonials text-center justify-center items-center gap-6 rounded-md">
-      <h3 className="text-2xl font-bold font-alt">Get early acess today</h3>
+    <div className="flex flex-col mx-6 py-12 px-6 md:px-28 mt-32 md:mx-[17.5rem] bg-primary-testimonials text-center justify-center items-center gap-6 rounded-md shadow-2xl">
+      <h3 className="text-xl md:text-2xl font-bold font-alt">Get early acess today</h3>
       <p>
         It only takes a minute to sign up and our free starter tier is extremely generous. If you have any questions,
         our support team would be happy to help you
       </p>
 
-      <div className="flex flex-row gap-6 w-full justify-between">
-        <input className="rounded-3xl px-6 w-2/3" type="text" name="" id="" placeholder="email@example.com" />
+      <div className="flex flex-col md:flex-row gap-6 w-full justify-between">
+        <input
+          className="rounded-3xl px-6 md:w-2/3 w-full py-4"
+          type="text"
+          name=""
+          id=""
+          placeholder="email@example.com"
+        />
         <button className="py-4 px-7 font-alt font-bold rounded-3xl bg-accent-blue hover:bg-accent-cyan">
           Get Started For Free
         </button>
--- src/components/productivePage.tsx
+++ src/components/productivePage.tsx
@@ -1,14 +1,11 @@
 import Image from "next/image";
 export function ProductPage() {
   return (
-    <section className="flex flex-row gap-12 px-32 py-32 justify-center">
+    <section className="flex flex-col md:flex-row gap-12 px-8 md:px-32 py-32 justify-center">
       <Image src="/illustration-stay-productive.png" alt="produtive illustration" width={615} height={465} />
 
-      <div className="flex flex-col text-left gap-8 justify-center">
-        <h2 className="text-3xl font-bold font-alt">
-          Stay productive,
-          <br /> wherever you are
-        </h2>
+      <div className="flex flex-col text-left gap-4 md:gap-8 justify-center">
+        <h2 className="text-xl md:text-3xl font-bold font-alt">Stay productive, wherever you are</h2>
         <p>
           Never let location be an issue when accessing your files. Fylo has you covered for all of your file storage
           needs.
@@ -17,10 +14,13 @@ export function ProductPage() {
           Securely sahre files and folders with friends, family and colleagues for live collaboration. No email
           attachments required.
         </p>
-        <a href="" className="text-accent-blue hover:text-accent-cyan underline underline-offset-2 inline-flex gap-1">
-          See how Fylo works <Image className="h-full w-fit" src="/icon-arrow.svg" alt="" width={16} height={16} />
-          <div></div>
-        </a>
+        <div className="flex flex-row items-start gap-1 ">
+          <a href="" className="text-accent-blue hover:text-accent-cyan">
+            See how Fylo works
+            <span className="block mt-1 max-w-full h-0.5 bg-accent-blue hover:bg-accent-cyan"></span>
+          </a>
+          <Image className="h-full w-fit" src="/icon-arrow.svg" alt="" width={16} height={16} />
+        </div>
       </div>
     </section>
   );
